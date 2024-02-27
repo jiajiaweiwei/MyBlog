544f4e809487193d484907f428abda10284a5d4b
ljw2088 <118275178+ljw2088@users.noreply.github.com>
2024-02-27 13:52:18 +0800

    创建主要模块

---
+++ MyBlog-Main/src/main/java/com/jiawei/BlogMain.java
@@ -0,0 +1,11 @@
+package com.jiawei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMain {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogMain.class,args);
+    }
+}
