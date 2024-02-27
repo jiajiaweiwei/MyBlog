ec9b6d7bccc4a0562391ee6e4000a052076605ce
ljw2088 <118275178+ljw2088@users.noreply.github.com>
2024-02-27 17:42:08 +0800

    测试1

---
+++ MyBlog-framework/src/main/java/com/jiawei/domain/entity/BlogArticle.java
@@ -0,0 +1,51 @@
+package com.jiawei.domain.entity;
+
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+/**
+ * 文章表(BlogArticle)表实体类
+ *
+ * @author makejava
+ * @since 2024-02-27 17:40:20
+ */
+@SuppressWarnings("serial")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BlogArticle  {
+
+    private Long id;
+//标题
+    private String title;
+//文章内容
+    private String content;
+//文章摘要
+    private String summary;
+//所属分类id
+    private Long categoryId;
+//缩略图
+    private String thumbnail;
+//是否置顶（0否，1是）
+    private String isTop;
+//状态（0已发布，1草稿）
+    private String status;
+//访问量
+    private Long viewCount;
+//是否允许评论 1是，0否
+    private String isComment;
+
+    private Long createBy;
+
+    private Date createTime;
+
+    private Long updateBy;
+
+    private Date updateTime;
+//删除标志（0代表未删除，1代表已删除）
+    private Integer delFlag;
+
+
+}
+
